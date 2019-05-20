208e0d0b7f77a9970393d7d57c9b99abd88ee5f1
thongnm <thongnm@allexceed.co.jp>
2019-05-20 13:24:16 +0700

    commit channge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from server.server import app
 
 if __name__ == '__main__':
-    app.run(host='192.168.10.114', port=9000, ssl_context="adhoc", debug=True)
+    app.run(host='192.168.10.114', port=8000, ssl_context="adhoc", debug=True)
